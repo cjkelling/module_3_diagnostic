c5cba1c2d187ad66b21531af9f20ce2d73e7762e
Christopher Kelling <cj.kelling88@gmail.com>
2019-12-09 11:20:05 -0700

    Create search model that connects to the service and returns all characters. The model also has a method for counting all characters.

---
+++ app/models/search.rb
@@ -0,0 +1,25 @@
+class Search
+  def initialize(house)
+    @house = house
+  end
+
+  def character_count
+    characters.count
+  end
+
+  def characters
+    service = PotterService.new
+    all_characters = service.characters
+
+    order_characters = []
+
+    all_characters.each do |character|
+      if character[:house] == @house
+        if character[:orderOfThePhoenix] == true
+          order_characters << Character.new(character)
+        end
+      end
+    end
+    order_characters
+  end
+end
